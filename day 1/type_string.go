e0e9d796835286cd699b94935ad467cd377cacde
M. Zulfadli Simatupang <zulfadlisimatupang@ecampus.ut.ac.id>
2021-05-10 12:36:24 +0700

    day 1

---
+++ day 1/type_string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ipay")
+	fmt.Println("Fadli")
+	fmt.Println("Muhammad Zulfadli")
+
+	// Function string
+	fmt.Println(len("Ipay"))
+	fmt.Println("Fadli"[4])
+	fmt.Println("Muhammad Zulfadli"[2])
+}
---
+++ day 1/variable.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// inisialisasi tipe data terlebih dahulu
+	var nama string
+
+	nama = "Muhammad Zulfadli"
+
+	fmt.Println(nama)
+
+	nama = "Muhammad Zulfadli Simatupang"
+
+	fmt.Println(nama)
+
+	// Ketika tidak di inisialisasi
+	var myGirlName = "Ayana"
+
+	fmt.Println(myGirlName)
+
+	var age = 24
+	fmt.Println(age)
+
+	// tipe data tanpa menulis kata kunci var
+	musicGenre := "Grunge"
+
+	fmt.Println(musicGenre)
+
+	// jika ingin  merubah suatu variable tidak bisa menggunaka ':=', karena itu hanya berlaku untuk deklarasi awal saja
+	musicGenre = "Pop Punk"
+	fmt.Println(musicGenre)
+}
